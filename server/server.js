9c7274ee7af16b87740b22fa2c3639bd43abea84
Dylan Reitsma <dylreitsma@gmail.com>
2024-02-29 14:07:21 -0500

    servers up

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql = require('mysql')
+const cors = require('cors')
 
 const app = express()
 app.use(cors())
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password:"fx-991ES:SQL",
+    user:"root",
+    password:"password",
     database:"mydb"
 })
 
 app.get('/',(re,res)=> {
-    return res.json("hello from the other side");
+    return res.json("hi");
 })
 
-app.get('/company',(req,res)=> {
+app.get('/company', (req,res)=>{
     const sql = "SELECT * FROM company";
     db.query(sql,(err,data)=> {
         if(err) return res.json(err);
-        return res.json(err)
+        return res.json(data);
     })
 })
 
-app.listen(8081, ()=> {
+app.listen(8080,()=> {
     console.log("yuh");
 })
